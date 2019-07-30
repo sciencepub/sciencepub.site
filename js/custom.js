82ea6bcdb646aefa93a9044dd523b3a6190df24e
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2019-07-30 19:50:07 -0300

    Init

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+/**
+ * Loads data from a src
+ *
+ * @returns Object
+ */
+function loadData() {
+    // return
+}
+
+/**
+ * Global scope
+ */
+
+
+function spEditions() {
+    // $.getJSON("data.json").done(
+    //     json => {
+    //         return ;
+    //     });
+
+
+    console.log(window.editions);
+}
+
+function spCities(data) {
+
+}
+
